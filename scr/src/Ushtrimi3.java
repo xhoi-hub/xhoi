8e5d9fc609eac80c6fa91834e229ed0b0f09694e
ASUS <Xhuendhysa@gmail.com>
2020-10-10 13:22:53 +0200

    first commit

--- scr/src/Ushtrimi3.java
+++ scr/src/Ushtrimi3.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
-    public class Ushtrimi3 {
+
+public class Ushtrimi3 {
         public static void main(String[] args) {
             Scanner in = new Scanner(System.in);
             System.out.print("\nvendos fjalen: ");
@@ -14,3 +15,5 @@ import java.util.Scanner;
         }
 
     }
+
+
