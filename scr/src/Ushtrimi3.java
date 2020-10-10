32641182383ecbd5384edaa2c67bf1541a2d5088
ASUS <Xhuendhysa@gmail.com>
2020-10-10 13:13:03 +0200

    String builder

---
+++ scr/src/Ushtrimi3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+    public class Ushtrimi3 {
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            System.out.print("\nvendos fjalen: ");
+            String word = in.nextLine();
+            word = word.trim();
+            String result = "";
+            char[] ch = word.toCharArray();
+            for (int i = ch.length - 1; i >= 0; i--) {
+                result += ch[i];
+            }
+            System.out.println("fjala e ndryshuar: " + result.trim());
+        }
+
+    }
